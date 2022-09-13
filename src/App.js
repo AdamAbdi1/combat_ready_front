ccf96612a1eae5b55f5df85f6944c8ea2de23468
sdam abdi <sdamabdi@sdams-MacBook-Pro.local>
2022-09-13 13:25:51 -0500

    compare

--- src/App.js
+++ src/App.js
@@ -1,52 +1,93 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './App.css';
+import Compare from './components/compare';
 
 const App = () => {
 
   let [combatant, setCombatant] = useState([])
   let [superHero, setSuperHero] = useState([])
+  let [hide, setHide] = useState('false')
+  let [player1, setPlayer1] = useState([])
 
-  const getCombatant = () => {
-    axios.get('http://localhost:8000/api/combatant')
-      .then(
-        (response) => setCombatant(response.data),
+
+  // const getCombatant = () => {
+  //   axios.get('http://localhost:8000/api/combatant')
+  //     .then(
+  //       (response) => setCombatant(response.data),
+  //       (err) => console.error(err)
+  //     )
+  //     .catch((error) => console.error(error))
+  // }
+
+  // useEffect(() => {
+  //   getCombatant()
+  // }, [])
+
+  // const getSuperHero = () => {
+  //   axios.get('https://akabab.github.io/superhero-api/api/all.json')
+  //     .then(
+  //       (response) => setSuperHero(response.data),
+  //       (err) => console.error(err)
+  //     )
+  //     .catch((error) => console.error(error))
+  // }
+
+  // useEffect(() => {
+  //   getSuperHero()
+  // }, [])
+
+  const getPlayer1 = () => {
+
+    axios.get('https://akabab.github.io/superhero-api/api/id/69.json')
+      .then((response) => setPlayer1(response.data),
         (err) => console.error(err)
       )
       .catch((error) => console.error(error))
   }
 
   useEffect(() => {
-    getCombatant()
+    getPlayer1()
   }, [])
 
-  const getSuperHero = () => {
-    axios.get('https://akabab.github.io/superhero-api/api/all.json')
-      .then(
-        (response) => setSuperHero(response.data),
-        (err) => console.error(err)
-      )
-      .catch((error) => console.error(error))
+  
+
+
+  let truefalse = () => {
+    if (hide === 'false') {
+      setHide('true')
+    }
+    if (hide === 'true') {
+      setHide('false')
+    }
   }
 
-  useEffect(() => {
-    getSuperHero()
-  }, [])
+
 
   return (
     <>
-      <h1>list of superhero's</h1>
-      {superHero.map((superheros) => {
-        return(
+      <div className="dropdown">
+        <button className="dropbtn">Dropdown</button>
+        <div className="dropdown-content">
+          <a href="#" onClick={truefalse}>List of Heros</a>
+        </div>
+      </div>
+      
+      {hide === 'false' ? <p hidden></p> : superHero.map((superheros) => {
+        return (
           <div key={superheros.id}>
-          <hr/>
-          <img src={superheros.images.sm}/>
-          <h4>Name: {superheros.name}</h4>
-          <h4>intelligence: {superheros.powerstats.intelligence}</h4>
-          <h4>strength: {superheros.powerstats.strength}</h4>
-          <h4>speed: {superheros.powerstats.speed}</h4>
-          <h4>durability: {superheros.powerstats.durability}</h4>
-          <h4>power: {superheros.powerstats.power}</h4>
-          <h4>combat: {superheros.powerstats.combat}</h4>
+            <hr />
+            <img src={superheros.images.sm} />
+            <h4>Name: {superheros.name}</h4>
+            <h4>Power Stats</h4>
+            <ul>
+              <li>intelligence: {superheros.powerstats.intelligence}</li>
+              <li>strength: {superheros.powerstats.strength}</li>
+              <li>speed: {superheros.powerstats.speed}</li>
+              <li>durability: {superheros.powerstats.durability}</li>
+              <li>power: {superheros.powerstats.power}</li>
+              <li>combat: {superheros.powerstats.combat}</li>
+            </ul>
           </div>
         )
       })}
